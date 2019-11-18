78f0c9cfc3e6b48dd39be9a1988528a522e183e9
Thomas Gredig <thomas.gredig@csulb.edu>
2019-11-17 16:34:00 -0800

    table with results

--- NTE.readParameters.R
+++ NTE.readParameters.R
@@ -4,6 +4,7 @@
 
 # CONFIGURATION
 source('config.R')
+file.PREFIX = 'NTE.readParameters'
 file.list = file.path(path.RAW, dir(path.RAW, pattern='_NTE_', recursive = TRUE))
 
 
@@ -16,21 +17,37 @@ library(pspline)
 library(cowplot)
 source('func.R')
 
-filename = file.list[3]
-d = NTE.load(file.list[3])
-g1 = NTE.plotSummary(d, sz=1)
-print(g1)
-ggsave('images/plotSummary.png', width=4,height=3, dpi=300)
-
-d2 = NTE.depStartEnd(d)
-g2 = NTE.plotSummary(d2, sz=1)
-print(g2)
-ggsave('images/plotDeposition.png', width=4,height=3, dpi=300)
-
-# find relevant parameters
-summary(d2)
-NTE.getParams(d)
-
-g3 = NTE.plotDeposition(d)
-print(g3)
-ggsave('images/plotDepositionColor.png', width=4,height=3, dpi=300)
+r = data.frame()
+no = 0
+for(filename in file.list) {
+  no = no+1
+  user = strsplit(filename,'_')[[1]][3]
+  sample.name = strsplit(filename,'_')[[1]][5]
+  print(paste("Sample:",sample.name))
+  d = NTE.load(filename)
+  #g1 = NTE.plotSummary(d, sz=1)
+  #print(g1)
+  #ggsave('images/plotSummary.png', width=4,height=3, dpi=300)
+  
+  d2 = NTE.depStartEnd(d)
+  if (!(is.na(d2))) {
+    #g2 = NTE.plotSummary(d2, sz=1)
+    #print(g2)
+    #ggsave('images/plotDeposition.png', width=4,height=3, dpi=300)
+    
+    # find relevant parameters
+    #summary(d2)
+    r1 = NTE.getParams(d)
+    r1$user = user
+    r1$sample = sample.name
+    r = rbind(r, r1)
+    
+    g3 = NTE.plotDeposition(d)
+    ggsave2(plot = g3, file = file.path(path.FIGS,paste0(file.PREFIX,'-',no,'-',sample.name,'.png')),
+           width=6, height=4, dpi=300)
+  }
+  #ggsave('images/plotDepositionColor.png', width=4,height=3, dpi=300)
+  
+}
+r
+write.csv(r, file = file.path(path.RESULTS, paste0(file.PREFIX,'-sampleList-NTE.csv')))
